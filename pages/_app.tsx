6044c04d73012c22efa41629e9f589f4cef02e93
Damien Deloubes <deloubesd@gmail.com>
2026-02-10 15:34:42 +0100

    add simple analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,11 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/thumbs";
 
-import '../public/css/bootstrap.min.css';
-import '../public/css/style.css';
+import "../public/css/bootstrap.min.css";
+import "../public/css/style.css";
 
 function MyApp({ Component, pageProps }) {
-
-    return (<Component {...pageProps} />)
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
